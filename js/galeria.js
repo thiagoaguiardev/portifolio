df1488560411d2c9dc4bc9a00ae727226daa45c1
thiagoaguiardev <thiagoaguiar.dev@gmail.com>
2022-08-05 23:40:39 -0300

    at03 corrigindo js
    Corrigindo a origens das pastas no js

--- js/galeria.js
+++ js/galeria.js
@@ -3,12 +3,12 @@ let close = document.querySelector('.close');
 for(let i  = 0; i <smallImg.length; i++){
     smallImg[i].addEventListener('click', function(){
         if (i == 0){
-            window.open('img1.html', '_parent');
+            window.open('../html/img1.html', '_parent');
         }else{
-            window.open('img2.html', '_parent');
+            window.open('../html/img2.html', '_parent');
         };
     });
 };
 close.addEventListener('click', function(){
-    window.open('index.html#projetos', '_parent');
+    window.open('../index.html#projetos', '_parent');
 });
